50a919c46cd9b3f82f181849f431ad580d256839
Landon Burnier <landonburnier@gmail.com>
2019-04-23 09:19:37 -0400

    button functionality

--- src/components/board.js
+++ src/components/board.js
@@ -1,54 +1,62 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../css/Board.css";
-import Note from './Note'
+import Note from "./Note";
 
 class Board extends Component {
-    constructor() {
-        super();  //inherits parent's stuff
-        this.state = { //state has data that CAN change
-            notes: [
-                {
-                    title: "Class Notes",
-                    body: "Use constructors when extending another class"
-                },
-                {
-                    title: "My Address", body: "Nunya"
-                },
-                {
-                    title: "React Notes", body: "Everything in React is a component"
-                }
-            ]
-        }
-    }
-    render() {
-        return (
-        <div>
-            <div className="div-board">
-                <div className="row">
-                {
-                    this.state.notes.map(note => {
-                        return <Note title={note.title} body={note.body} />
-                    })
-                }
-                </div>
-            </div>
-            <button className="btn btn-success add-button" onClick={this.addNote.bind(this)}>Add</button>
-        </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      notes: []
+    };
+  }
 
-
-addNote() {
-    this.state.notes.push(
-        {
-            title: "New Notes Title",
-            body: "New Body Title"
-        }
-    )
+  addNote() {
+    this.state.notes.push({
+      id: Date.now()
+    });
     this.setState({
-        notes: this.state.notes
-    })
+      notes: this.state.notes
+    });
+  }
+
+  deleteNote(id) {
+    let newNoteArr = this.state.notes;
+    newNoteArr.map((note, index) => {
+      if (id === note.id) {
+        newNoteArr.splice(index, 1);
+      }
+    });
+    this.setState({
+      notes: newNoteArr
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="div-board">
+          <div className="row">
+            {this.state.notes.map(note => {
+              return (
+                <Note
+                  key={note.id}
+                  id={note.id}
+                  deleteHandler={this.deleteNote.bind(this)}
+                />
+              );
+            })}
+          </div>
+          <div>
+            <button
+              className="btn btn-success add-button"
+              onClick={this.addNote.bind(this)}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
-
 export default Board;
